c1abb7883fb2ffb2b85fb33adc51461ddb47fbe9
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-08-29 14:36:49 +0200

    added previous and next post navigation to single posts

--- inc/template-tags.php
+++ inc/template-tags.php
@@ -314,6 +314,24 @@ if ( ! function_exists( 'codename_more_link' ) ) :
 endif;
 
 
+if ( ! function_exists( 'codename_post_navigation' ) ) :
+	/**
+	 * Displays Single Post Navigation
+	 */
+	function codename_post_navigation() {
+
+		if ( true === codename_get_option( 'post_navigation' ) || is_customize_preview() ) {
+
+			the_post_navigation( array(
+				'prev_text' => '<span class="nav-link-text">' . esc_html_x( 'Previous Post', 'post navigation', 'codename' ) . '</span><h3 class="entry-title">%title</h3>',
+				'next_text' => '<span class="nav-link-text">' . esc_html_x( 'Next Post', 'post navigation', 'codename' ) . '</span><h3 class="entry-title">%title</h3>',
+			) );
+
+		}
+	}
+endif;
+
+
 if ( ! function_exists( 'codename_pagination' ) ) :
 	/**
 	 * Displays pagination on archive pages
