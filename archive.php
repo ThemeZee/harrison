fb126c7b7611f2a76d93e44d13c5d66dd709c7d4
Thomas Weichselbaumer <kontakt@themecoder.de>
2019-11-11 11:39:03 +0100

    fixed layout issue on archive pages when grid blog layout is active

--- archive.php
+++ archive.php
@@ -15,18 +15,18 @@ if ( have_posts() ) :
 
 	<main id="main" class="site-main" role="main">
 
+		<?php codename_archive_header(); ?>
+
 		<div id="post-wrapper" class="post-wrapper">
 
-			<?php
-			codename_archive_header();
+		<?php
+		while ( have_posts() ) :
+			the_post();
 
-			while ( have_posts() ) :
-				the_post();
+			get_template_part( 'template-parts/blog/content', esc_html( codename_get_option( 'blog_layout' ) ) );
 
-				get_template_part( 'template-parts/blog/content', esc_html( codename_get_option( 'blog_layout' ) ) );
-
-			endwhile;
-			?>
+		endwhile;
+		?>
 
 		</div>
 
